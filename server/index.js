de7947e5d1823be2fd74796f5c3e8e65ae412a9a
rbabaci1 <4rabah@gmail.com>
2023-03-12 10:30:27 -0700

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config(); // load .env file
 
 const app = express(); // create express app
@@ -12,6 +14,12 @@ app.get('/', (req, res) => {
   res.send('Hello from DALL-E!');
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Server started on port ${process.env.PORT}`);
-});
+try {
+  connectDB(process.env.MONGODB_URL);
+
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
---
+++ server/mongodb/connect.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = url => {
+  mongoose.set('strictQuery', true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => {
+      console.error('Failed to connect with MongoDB');
+      console.error(err);
+    });
+};
+
+export default connectDB;
