9de1c13d5b76e08bcda5955c55669f786af91788
rbabaci1 <4rabah@gmail.com>
2023-03-12 10:49:51 -0700

    implement the dalle routes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+// import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config(); // load .env file
 
 const app = express(); // create express app
 app.use(cors()); // enable cors
 app.use(express.json({ limit: '50mb' })); // enable json body parsing
+
+// routes
+// app.use('/api/posts', postRoutes);
+app.use('/api/dalle', dalleRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from DALL-E!');
