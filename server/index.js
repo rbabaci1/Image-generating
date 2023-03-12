3c4762594dfd06ab27c4cd3ef145d79adde16a4d
rbabaci1 <4rabah@gmail.com>
2023-03-12 10:51:18 -0700

    implement the post routes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '50mb' })); // enable json body parsing
 
 // routes
 // app.use('/api/posts', postRoutes);
-app.use('/api/dalle', dalleRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from DALL-E!');
